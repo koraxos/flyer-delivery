6fe403ad03a1199411e064c0d04c96cbcdf3c69f
WemiceD <georgin.aymeric@gmail.com>
2019-03-19 20:12:45 +0100

    <Test> try change db

--- config/seed.js
+++ config/seed.js
@@ -1,64 +1,69 @@
-'use strict';
+"use strict";
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Promise = require("bluebird");
-var shuffle = require('shuffle-array');
+var shuffle = require("shuffle-array");
 
-var config = require('./environment');
+var config = require("./environment");
 var path = require("path"),
-    csv = require('csv'),
-    parse = require('csv-parse'),
-    fs = require("fs");
+  csv = require("csv"),
+  parse = require("csv-parse"),
+  fs = require("fs");
 
-var Client = require(path.join(config.root, "schema","client")),
-  Place = require(path.join(config.root, "schema","place")),
-  Commande = require(path.join(config.root, "schema","commande")),
-  Zone = require(path.join(config.root, "schema","zone"));
+var Client = require(path.join(config.root, "schema", "client")),
+  Place = require(path.join(config.root, "schema", "place")),
+  Commande = require(path.join(config.root, "schema", "commande")),
+  Zone = require(path.join(config.root, "schema", "zone"));
 
-
-Client.find({}).remove(function(err){
+Client.find({}).remove(function(err) {
   if (err) throw err;
   console.log("Clients removed");
 });
 
-
-Commande.find({}).remove(function(err){
+Commande.find({}).remove(function(err) {
   if (err) throw err;
   console.log("Commandes removed");
 });
 
+var parser = parse({}, function(err, data) {
+  if (err) console.warn(err);
+  var zones = new Array(),
+    promsZones = new Array();
+  data.forEach(function(zone) {
+    if (zone[0] == "Ville") return;
 
+    promsZones.push(
+      new Promise(function(resolve, reject) {
+        Zone.create({
+          Ville: zone[0],
+          region: zone[1],
+          brut: parseInt(zone[2]),
+          net: parseInt(zone[3])
+        }).then(
+          function(zon) {
+            resolve(zon);
+          },
+          function(err) {
+            reject(err);
+          }
+        );
+      })
+    );
 
-
-var parser = parse({}, function(err, data){
-  if(err) console.warn(err);
-  var zones = new Array(), promsZones = new Array();
-  data.forEach(function(zone){
-    if(zone[0]=="Ville") return;
-
-    promsZones.push(new Promise(function(resolve, reject){
-      Zone.create({Ville:zone[0], region:zone[1], brut:parseInt(zone[2]), net:parseInt(zone[3])}).then(function(zon){
-        resolve(zon);
-      }, function(err){
-        reject(err);
-      });
-    }));
-
-    Promise.all(promsZones).then(function(zones){
-      console.log(zones.length+" Zones créer");
-    }, function(err){
-      console.warn(err);
-    })
-    
+    Promise.all(promsZones).then(
+      function(zones) {
+        console.log(zones.length + " Zones créer 3");
+      },
+      function(err) {
+        console.warn(err);
+      }
+    );
   });
 });
 
-
-
-Zone.find({}).remove(function(err){
+Zone.find({}).remove(function(err) {
   if (err) throw err;
   console.log("Zones removed");
 
-  fs.createReadStream(config.root+"/public/data/prices.csv").pipe(parser);
+  fs.createReadStream(config.root + "/public/data/prices.csv").pipe(parser);
 });
-
